4a22f821346ba694eeafa42af68a284c4a4e1b02
Christoph Stahl <christoph.stahl@tu-dortmund.de>
2026-07-19 13:07:56 +0200

    more output

--- resources/flatpak/check-version.sh
+++ resources/flatpak/check-version.sh
@@ -3,4 +3,5 @@
 RELEASE_VERSION=$1
 FLATPAK_VERSION=$(grep "<release version=" resources/flatpak/rocks.syng.Syng.metainfo.xml | sed 's/^[^"]*"\([^"]*\)".*$/\1/' | sort -V | tail -n 1)
 
+echo expexted $FLATPAK_VERSION, found $RELEASE_VERSION
 [ "$RELEASE_VERSION" = "$FLATPAK_VERSION" ]
