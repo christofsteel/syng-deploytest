fcefe59ad6a462af0ddcd03f11d6119480be9ef5
Christoph Stahl <christoph.stahl@tu-dortmund.de>
2026-07-27 15:20:03 +0200

    revert latest win deps

--- resources/windows/latest-win-deps_pywine.sh
+++ resources/windows/latest-win-deps_pywine.sh
@@ -2,8 +2,6 @@
 
 . ../utils.sh
 
-echo get wine version 1>&2
-WINE_VERSION=$(get_latest_tag Kron4ek/Wine-Builds)
 echo get mpv version 1>&2
 MPV_NIGHTLY_URL="$(get_latest_release shinchiro/mpv-winbuild-cmake | grep mpv-dev-x86_64 | head -n 1)"
 MPV_NIGHTLY=$(get_tag_from_url "$MPV_NIGHTLY_URL")
@@ -11,19 +9,15 @@ MPV_HASH=$(get_hash_from_url "$MPV_NIGHTLY_URL")
 echo get ffmpeg version 1>&2
 FFMPEG_NIGHTLY_URL="$(get_latest_release shinchiro/mpv-winbuild-cmake | grep ffmpeg-x86_64 | head -n 1)"
 FFMPEG_HASH=$(get_hash_from_url "$FFMPEG_NIGHTLY_URL")
-echo get python version 1>&2
-PYTHON_VERSION=$(get_latest_python)
 echo get mono version 1>&2
 MONO_VERSION=$(get_latest_tag wine-mono/wine-mono | sed "s/wine-mono-\(.*\)/\1/")
 echo get deno version 1>&2
 DENO_VERSION=$(get_latest_tag denoland/deno | sed "s/v\(.*\)/\1/") 
 
 
-echo WINE_VERSION=$WINE_VERSION
 echo MPV_NIGHTLY=$MPV_NIGHTLY
 echo MPV_HASH=$MPV_HASH
 echo FFMPEG_HASH=$FFMPEG_HASH
-echo PYTHON_VERSION=$PYTHON_VERSION
 echo MONO_VERSION=$MONO_VERSION
 echo DENO_VERSION=$DENO_VERSION
 
